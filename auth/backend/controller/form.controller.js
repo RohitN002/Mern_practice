cb13e5426b353eddc79b01cda599d19ec8f7ae20
RohitN002 <rohitrandy002@gmail.com>
2024-05-12 21:16:53 +0530

    auth

--- auth/backend/controller/form.controller.js
+++ auth/backend/controller/form.controller.js
@@ -1,16 +1,22 @@
 const signupModel = require('../models/signupModel')
-
+const bcrypt = require('bcrypt')
 
 const signup=async(req,res)=>{
-    const {name,email,password} = req.body
+  
     try {
-const existingUser = await signupModel.findOne({email})
+        const {name,email,password} = req.body
+        const existingUser = await signupModel.findOne({email})
 if(existingUser){
    return  res.status(201).json({message:"User already exist"})
 }
-const newUser = signupModel({name,email,password})
-const savedUser = await newUser.save()
-return res.status(200).json({message:"Account created sucessfully"})
+        bcrypt.hash(password,10)
+        .then(async (hash)=>{
+            const newUser = signupModel({name,email,password:hash})
+            const savedUser = await newUser.save()
+            return res.status(200).json({message:"Account created sucessfully"})
+        })
+
+
     }catch(error){
  return res.status(500).json({message:"Internal server error"})
     }
@@ -23,8 +29,16 @@ const signin = async (req,res)=>{
         if (!existingUser){
             return res.status(401).json({message:"Mail id not registerd"})
  }
-        if(existingUser.password===password){
+        if(existingUser){
+            bcrypt.compare(existingUser.password,password,(err,response)=>{
+                if(err){
+                    return res.status(401).json({message:"error"})
+                }
+                if(response){
+                    
             return res.status(200).json({message:"Loggin sucessful"})
+                }
+            })
         }else{
             return res.status(401).json({message:"mail id or password doesnt match"})
         }

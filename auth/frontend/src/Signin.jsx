43732cf916fb60b61527acb9fdef4dac7bfb6042
RohitN002 <rohitrandy002@gmail.com>
2024-05-12 21:41:36 +0530

    JWT

--- auth/frontend/src/Signin.jsx
+++ auth/frontend/src/Signin.jsx
@@ -6,6 +6,7 @@ import {Link, useNavigate} from 'react-router-dom'
 const Signin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  axios.defaults.withCredentials=true
 const navigate=useNavigate()
   const handleChange = (event) => {
     const { name, value } = event.target;
